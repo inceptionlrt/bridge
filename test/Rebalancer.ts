e7c12516e654f3cb6aec3d7a33b1efe5f5374beb
Pavel Fomichov <pavel.f@tagus.xyz>
2024-08-22 11:48:50 +0100

    test update

--- test/Rebalancer.ts
+++ test/Rebalancer.ts
@@ -1,44 +1,32 @@
-import {
-    time,
-    loadFixture,
-} from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import { expect } from "chai";
 import hre from "hardhat";
 
 describe("Rebalancer and InETH Contracts", function () {
-    // We define a fixture to reuse the same setup in every test.
-    // We use loadFixture to run this setup once, snapshot that state,
-    // and reset Hardhat Network to that snapshot in every test.
     async function deployContractsFixture() {
         // Get signers
-        const [owner, addr1, addr2] = await hre.ethers.getSigners();
-
-        // Deploy InETH contract
-        const InETHFactory = await hre.ethers.getContractFactory("InETH");
-        const inETH = await InETHFactory.deploy(owner.address, addr1.address);
+        const [owner, addr1] = await hre.ethers.getSigners();
 
         // Deploy Rebalancer contract
         const RebalancerFactory = await hre.ethers.getContractFactory("Rebalancer");
         const rebalancer = await RebalancerFactory.deploy(owner.address);
 
+        const rebalancerAddress = await rebalancer.getAddress();
+        
+        // Deploy InETH contract
+        const InETHFactory = await hre.ethers.getContractFactory("InETH");
+        const inETH = await InETHFactory.deploy(owner.address, rebalancerAddress);
+        const inEthAddress = await inETH.getAddress();
+
+
+
         // Set InETH address in Rebalancer
-        await rebalancer.setInETHAddress(inETH.address);
+        await rebalancer.setInETHAddress(inEthAddress);
         // Set Rebalancer address in InETH
-        await inETH.setRebalancer(rebalancer.address);
+        await inETH.setRebalancer(rebalancerAddress);
 
-        return { inETH, rebalancer, owner, addr1, addr2 };
+        return { inETH, rebalancer, owner };
     }
-
-    describe("Deployment", function () {
-        it("Should deploy both contracts correctly", async function () {
-            const { inETH, rebalancer } = await loadFixture(deployContractsFixture);
-
-            expect(inETH.address).to.properAddress;
-            expect(rebalancer.address).to.properAddress;
-        });
-
-
-    });
 
     describe("getRatio() Function", function () {
         it("Should return correct getRatio() when ETH = inETH", async function () {
@@ -48,7 +36,7 @@ describe("Rebalancer and InETH Contracts", function () {
             await rebalancer.mintAdmin(amount, amount);
             const ratio = await rebalancer.getRatio();
 
-            expect(ratio).to.equal(hre.ethers.toBigInt(1e18)); // Ratio should be 1 (ETH/inETH = 1)
+            expect(ratio).to.equal(hre.ethers.toBigInt((BigInt(1000) * BigInt("1000000000000000000")) / BigInt(1000)));
         });
 
         it("Should return correct getRatio() when ETH > inETH", async function () {
@@ -59,7 +47,7 @@ describe("Rebalancer and InETH Contracts", function () {
             await rebalancer.mintAdmin(inEthAmount, ethAmount);
             const ratio = await rebalancer.getRatio();
 
-            expect(ratio).to.equal(hre.ethers.toBigInt((1000n * 1e18n) / 1500n)); // Ratio should be < 1
+            expect(ratio).to.equal(hre.ethers.toBigInt((BigInt(1000) * BigInt("1000000000000000000")) / BigInt(1500)));
         });
 
         it("Should return correct getRatio() when ETH < inETH", async function () {
@@ -70,7 +58,7 @@ describe("Rebalancer and InETH Contracts", function () {
             await rebalancer.mintAdmin(inEthAmount, ethAmount);
             const ratio = await rebalancer.getRatio();
 
-            expect(ratio).to.equal(hre.ethers.toBigInt((1500n * 1e18n) / 1000n)); // Ratio should be > 1
+            expect(ratio).to.equal(hre.ethers.toBigInt((BigInt(1500) * BigInt("1000000000000000000")) / BigInt(1000)));
         });
 
         it("Should return correct getRatio() when ETH >> inETH (much higher)", async function () {
@@ -81,7 +69,7 @@ describe("Rebalancer and InETH Contracts", function () {
             await rebalancer.mintAdmin(inEthAmount, ethAmount);
             const ratio = await rebalancer.getRatio();
 
-            expect(ratio).to.equal(hre.ethers.toBigInt((100n * 1e18n) / 10000n)); // Ratio should be much < 1
+            expect(ratio).to.equal(hre.ethers.toBigInt((BigInt(100) * BigInt("1000000000000000000")) / BigInt(10000)));
         });
 
         it("Should return correct getRatio() when ETH << inETH (much lower)", async function () {
@@ -92,7 +80,7 @@ describe("Rebalancer and InETH Contracts", function () {
             await rebalancer.mintAdmin(inEthAmount, ethAmount);
             const ratio = await rebalancer.getRatio();
 
-            expect(ratio).to.equal(hre.ethers.toBigInt((10000n * 1e18n) / 100n)); // Ratio should be much > 1
+            expect(ratio).to.equal(hre.ethers.toBigInt((BigInt(10000) * BigInt("1000000000000000000")) / BigInt(100)));
         });
 
         it("Should return correct getRatio() when ETH == 0 || inETH == 0", async function () {
@@ -103,7 +91,7 @@ describe("Rebalancer and InETH Contracts", function () {
             await rebalancer.mintAdmin(inEthAmount, ethAmount);
             const ratio = await rebalancer.getRatio();
 
-            expect(ratio).to.equal(hre.ethers.toBigInt(1e18)); // Ratio should be 1 (by default condition)
+            expect(ratio).to.equal(hre.ethers.toBigInt("1000000000000000000"));
         });
 
         it("Should return correct getRatio() when ETH == 0 && inETH == 0", async function () {
@@ -114,8 +102,7 @@ describe("Rebalancer and InETH Contracts", function () {
             await rebalancer.mintAdmin(inEthAmount, ethAmount);
             const ratio = await rebalancer.getRatio();
 
-            expect(ratio).to.equal(hre.ethers.toBigInt(1e18)); // Ratio should be 1 (by default condition)
+            expect(ratio).to.equal(hre.ethers.toBigInt("1000000000000000000"));
         });
     });
 });
-
