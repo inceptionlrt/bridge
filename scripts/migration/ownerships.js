1baedbdab2b6a6cb8cb55afb57d1ef779be43079
davosloper <davosloper@gmail.com>
2024-10-15 16:11:44 +0500

    configuration

--- scripts/migration/ownerships.js
+++ scripts/migration/ownerships.js
@@ -10,28 +10,28 @@ const deployFactory = async () => {
   const [deployer] = await ethers.getSigners();
   await printBalance(deployer);
 
-  if (hre.network.name == "base") {
+  if (hre.network.name == "fraxtal") {
 
-    console.log("BASE")
-    let baseMultisig = "0x03e2157773e48d59592C26EC5B6F976D85622Cce";
-    let paBridge = "0xB81e55e7Ee6B286aF6abFEa4eFad83f7BA4D1f1e", prBridge = "0xC00cD5599F7E128FC5Ed5563147a45B12e83B3ac";
-    let paX = "0x67f199841416388eeAd3bc48178c36651579FA3A", prX = "0xb655932EE66A3C609D57cC24309a0e2c594C944e";
+    console.log("FRAXTAL")
+    let fraxtalMultisig = "0xc95AB1e2253a1d93cec51ad862ABfA130c14361F";
+    let paBridge = "0xB5C479CC2Ee8D24b1aE86ac270598F1a571abd6B", prBridge = "0xC00cD5599F7E128FC5Ed5563147a45B12e83B3ac";
+    let paX = "0x64a6c90871B774C1678dDBC48D99040b03a9b84d", prX = "0xE162075a1C0Ac7e985253972bEcA5e83Da3BBaa4";
 
     let paBridgeC = await ethers.getContractAt("ProxyAdmin", paBridge);
     let prBridgeC = await ethers.getContractAt("ProxyAdmin", prBridge);
     let paXC = await ethers.getContractAt("ProxyAdmin", paX);
     let prXc = await ethers.getContractAt("ProxyAdmin", prX);
 
-    let TX = await paBridgeC.transferOwnership(baseMultisig); await TX.wait(); console.log("1");
-    TX = await prBridgeC.transferOwnership(baseMultisig); await TX.wait(); console.log("2");
-    TX = await paXC.transferOwnership(baseMultisig); await TX.wait(); console.log("3");
-    TX = await prXc.transferOwnership(baseMultisig); await TX.wait(); console.log("4");
+    let TX = await paBridgeC.transferOwnership(fraxtalMultisig); await TX.wait(); console.log("1");
+    TX = await prBridgeC.transferOwnership(fraxtalMultisig); await TX.wait(); console.log("2");
+    TX = await paXC.transferOwnership(fraxtalMultisig); await TX.wait(); console.log("3");
+    TX = await prXc.transferOwnership(fraxtalMultisig); await TX.wait(); console.log("4");
   }
   else if (hre.network.name == "ethereum") {
 
     console.log("ETHEREUM")
     let ethereumMultisig = "0x8e6C8799B542E507bfDDCA1a424867e885D96e79";
-    let paX = "0x10D9A419478FeE5aa35c9f3b36B37025E9Ff8110", prX = "0xb655932EE66A3C609D57cC24309a0e2c594C944e";
+    let paX = "0xef6f479dBE4eaA80eEa939d35fc0638c12473264", prX = "0xE162075a1C0Ac7e985253972bEcA5e83Da3BBaa4";
 
     let paXC = await ethers.getContractAt("ProxyAdmin", paX);
     let prXc = await ethers.getContractAt("ProxyAdmin", prX);
