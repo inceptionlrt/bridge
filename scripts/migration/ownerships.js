11c3fbad5233bbab1a65efca59756ca38a2b7055
davosloper <davosloper@gmail.com>
2024-10-21 14:50:00 +0500

    ownerships

--- scripts/migration/ownerships.js
+++ scripts/migration/ownerships.js
@@ -26,17 +26,30 @@ const deployFactory = async () => {
     // TX = await prBridgeC.transferOwnership(fraxtalMultisig); await TX.wait(); console.log("2");
     TX = await paXC.transferOwnership(fraxtalMultisig); await TX.wait(); console.log("3");
     TX = await prXc.transferOwnership(fraxtalMultisig); await TX.wait(); console.log("4");
+  } else if (hre.network.name == "bsc") {
+
+    console.log("BSC")
+    let bscMultisig = "0x8aAF28382A7D0954Bbe99AE7E34304169FF758f9";
+    let paX = "0xB2F44773e99cfFeCb00AE9ba62913EA14C3B6163";
+    let prX = "0xB2B446386633C6746B0a2735FB57edBb066c5878";
+
+    let paXC = await ethers.getContractAt("ProxyAdmin", paX);
+    let prXc = await ethers.getContractAt("ProxyAdmin", prX);
+
+    TX = await paXC.transferOwnership(bscMultisig); await TX.wait(); console.log("3");
+    TX = await prXc.transferOwnership(bscMultisig); await TX.wait(); console.log("4");
   }
   else if (hre.network.name == "ethereum") {
 
     console.log("ETHEREUM")
     let ethereumMultisig = "0x8e6C8799B542E507bfDDCA1a424867e885D96e79";
-    let paX = "0xCdD6b2e8E43c4281F99c44A316bACC3348A873A4", prX = "0x157743261C3ba961e92421b268A881AeCe450d41";
+    // let paX = "0xCdD6b2e8E43c4281F99c44A316bACC3348A873A4", 
+    let prX = "0xB2B446386633C6746B0a2735FB57edBb066c5878";
 
-    let paXC = await ethers.getContractAt("ProxyAdmin", paX);
+    // let paXC = await ethers.getContractAt("ProxyAdmin", paX);
     let prXc = await ethers.getContractAt("ProxyAdmin", prX);
 
-    TX = await paXC.transferOwnership(ethereumMultisig); await TX.wait(); console.log("1");
+    // TX = await paXC.transferOwnership(ethereumMultisig); await TX.wait(); console.log("1");
     TX = await prXc.transferOwnership(ethereumMultisig); await TX.wait(); console.log("2");
   } else throw ("Incorrect Network");
 };
