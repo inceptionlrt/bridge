a4b479a76e1427f4e4738cf7b13029fe109d582b
davosloper <davosloper@gmail.com>
2024-12-12 14:52:15 +0500

    add/resume_2

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,6 +10,7 @@ require("@openzeppelin/hardhat-upgrades");
 
 require("./tasks/setup-bridge");
 require("./tasks/deploy-xerc20");
+require("./tasks/deploy-xerc20-i");
 require("./tasks/deploy-rate-provider");
 
 /** @type import('hardhat/config').HardhatUserConfig */
